d27315d935da40e356320454f7b3645d5f6d9b67
KangWonBeen <WonKong01@naver.com>
2025-03-25 18:16:52 +0900

    250325 reservation 5차

--- src/socar/main/AppController.java
+++ src/socar/main/AppController.java
@@ -2,28 +2,37 @@ package socar.main;
 
 import socar.car.service.CarService;
 import socar.common.AppService;
+import socar.reservation.service.ReservationService;
 import socar.user.service.UserService;
 
 public class AppController {
 
     private AppService service;
-    private UserService userService = new UserService();  // UserService 인스턴스
+    private UserService userService;
+    private String loggedInUserId;  // 로그인된 사용자 아이디
+
+    public AppController() {
+        // AppController 생성 시 UserService 인스턴스를 넘겨줍니다.
+        this.userService = new UserService(this); // AppController 전달
+    }
 
     // 선택한 메뉴에 따라 시스템을 정해주는 기능
     public void chooseSystem(int selectNumber) {
         switch (selectNumber) {
             case 1:
-                service = new UserService();
-                break;
-//            case 2:
-//                service = new ReserationsService();
-//                break;
-            case 3:
-                String loggedInUserId = getLoggedInUserId();
-                if("admin".equals(loggedInUserId)) {
-                    service = new CarService();
+                if (userService.isLoggedIn()) {
+                    System.out.println("이미 로그인된 상태입니다.");
+                    return;
                 }
-                else {
+                service = new UserService(this);  // UserService에 AppController 전달
+                break;
+            case 2:
+                service = new ReservationService();
+                break;
+            case 3:
+                if ("admin".equals(loggedInUserId)) {
+                    service = new CarService();
+                } else {
                     System.out.println("관리자만 실행 가능합니다.");
                     return;
                 }
@@ -40,13 +49,14 @@ public class AppController {
         } catch (Exception e) {
             System.out.println("# 메뉴를 다시 입력하세요!");
         }
-
     }
-    // 로그인된 사용자 아이디를 반환하는 메서드
-    public String getLoggedInUserId() {
-        return ((UserService)service).getLoggedInUserId();  // 로그인된 사용자 아이디 반환
+
+    // 로그인된 사용자 아이디를 설정
+    public void setLoggedInUserId(String userId) {
+        this.loggedInUserId = userId;
     }
 
 }
+
 
 
